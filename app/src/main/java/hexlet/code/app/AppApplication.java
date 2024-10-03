dfc7f4297e50adccf3e4ba023033cb207adb9854
Kreoin <steamuser85@yandex.ru>
2024-10-03 20:16:30 +0300

    add Faker bean

--- app/src/main/java/hexlet/code/app/AppApplication.java
+++ app/src/main/java/hexlet/code/app/AppApplication.java
@@ -2,11 +2,19 @@ package hexlet.code.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import net.datafaker.Faker;
 
 @SpringBootApplication
 public class AppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
+    }
+
+    @Bean
+    public Faker getFaker() {
+        return new Faker();
     }
 }
