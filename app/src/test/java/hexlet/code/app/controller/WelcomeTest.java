b56c4e94b3654a68c33e2b2b34bc4caabb512b3c
Kreoin <steamuser85@yandex.ru>
2024-10-03 20:19:11 +0300

    add WelcomeTEst andd UserControllerTest

---
+++ app/src/test/java/hexlet/code/app/controller/WelcomeTest.java
@@ -0,0 +1,26 @@
+package hexlet.code.app.controller;
+
+import static org.hamcrest.Matchers.containsString;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class WelcomeTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+	@Test
+    public void testIndex() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(content().string(containsString("Welcome to Spring")));
+    }
+}
---
+++ app/src/test/java/hexlet/code/app/controller/api/UsersControllerTest.java
@@ -0,0 +1,128 @@
+package hexlet.code.app.controller.api;
+
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
+import org.instancio.Instancio;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.JwtRequestPostProcessor;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import hexlet.code.app.dto.UserDTO;
+import hexlet.code.app.mapper.UserMapper;
+import hexlet.code.app.model.User;
+import hexlet.code.app.repository.UserRepository;
+import hexlet.code.app.util.ModelGenerator;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UsersControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    
+    @Autowired
+    private WebApplicationContext wac;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ModelGenerator modelGenerator;
+
+    private JwtRequestPostProcessor token;
+
+    private User testUser;
+
+    @BeforeEach
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac)
+                .defaultResponseCharacterEncoding(StandardCharsets.UTF_8)
+                .apply(springSecurity())
+                .build();
+
+        token = jwt().jwt(builder -> builder.subject("hexlet@example.com"));
+
+        testUser = Instancio.of(modelGenerator.getUserModel())
+                .create();
+
+        userRepository.save(testUser);
+    }
+
+    @Test
+    public void testIndex() throws Exception {
+        var response = mockMvc.perform(get("/api/users")
+                .with(token))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse();
+        var body = response.getContentAsString();
+
+        List<UserDTO> userDTOS = objectMapper.readValue(body, new TypeReference<>() {});
+
+        var actual = userDTOS.stream().map(userMapper::map).toList();
+        var expected = userRepository.findAll();
+
+        Assertions.assertThat(actual).containsExactlyInAnyOrderElementsOf(expected);
+    }
+
+    @Test
+    public void testShow() throws Exception {
+        var request = get("/api/users/" + testUser.getId())
+                .with(token);
+        var result = mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse();
+        var body = result.getContentAsString();
+        assertThatJson(body).and(
+                v -> v.node("email").isEqualTo(testUser.getEmail()),
+                v -> v.node("firstName").isEqualTo(testUser.getFirstName()),
+                v -> v.node("lastName").isEqualTo(testUser.getLastName()));
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+        var data =  Instancio.of(modelGenerator.getUserModel()).create();
+
+        var request = post("/api/users")
+                .with(token)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(data));
+
+        mockMvc.perform(request)
+                .andExpect(status().isCreated());
+
+        var user =  userRepository.findByEmail(testUser.getEmail()).orElseThrow();
+
+        assertNotNull(user);
+        assertEquals(testUser.getFirstName(), user.getFirstName());
+        assertEquals(testUser.getLastName(), user.getLastName());
+        assertEquals(testUser.getEmail(), user.getEmail());
+    }
+}
