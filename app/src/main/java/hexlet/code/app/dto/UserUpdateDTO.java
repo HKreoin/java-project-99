8c2d5bfb6038e74e0a5267bf394ca0324b414823
Kreoin <steamuser85@yandex.ru>
2024-10-03 22:54:25 +0300

    fix UserUpdateDTO

--- app/src/main/java/hexlet/code/app/dto/UserUpdateDTO.java
+++ app/src/main/java/hexlet/code/app/dto/UserUpdateDTO.java
@@ -1,9 +1,5 @@
 package hexlet.code.app.dto;
 
-import jakarta.persistence.Column;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,16 +7,7 @@ import lombok.Setter;
 @Setter
 public class UserUpdateDTO {
 
-    @Email
-    @NotBlank
-    @Column(unique = true)
-    private String email;
-
     private String firstName;
 
     private String lastName;
-
-    @NotBlank
-    @Size(min = 3)
-    private String password;
 }
