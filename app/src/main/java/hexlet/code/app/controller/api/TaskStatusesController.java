56703ebb4723879d4e12cd6cd4c4615738e44924
Kreoin <steamuser85@yandex.ru>
2024-10-05 00:15:57 +0300

    creatt TaskStatus service and controller

---
+++ app/src/main/java/hexlet/code/app/controller/api/TaskStatusesController.java
@@ -0,0 +1,64 @@
+package hexlet.code.app.controller.api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import hexlet.code.app.dto.TaskStatusCreateDTO;
+import hexlet.code.app.dto.TaskStatusDTO;
+import hexlet.code.app.dto.TaskStatusUpdateDTO;
+import hexlet.code.app.service.TaskStatusService;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/task_statuses")
+public class TaskStatusesController {
+
+    @Autowired
+    private TaskStatusService service;
+
+    @GetMapping("")
+    @ResponseStatus(HttpStatus.OK)
+    ResponseEntity<List<TaskStatusDTO>> index() {
+        var dtos = service.findAll();
+        return ResponseEntity.ok()
+                .header("X-Total-Count", String.valueOf(dtos.size()))
+                .body(dtos);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    TaskStatusDTO show(@PathVariable Long id) {
+        return service.findById(id);
+    }
+
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    TaskStatusDTO create(@Valid @RequestBody TaskStatusCreateDTO data) {
+        return service.create(data);
+    }
+
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.OK)
+    TaskStatusDTO update(@PathVariable Long id, @Valid @RequestBody TaskStatusUpdateDTO data) {
+        return service.update(id, data);
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void destroy(@PathVariable Long id) {
+        service.delete(id);
+    }
+
+}
---
+++ app/src/main/java/hexlet/code/app/service/TaskStatusService.java
@@ -0,0 +1,56 @@
+package hexlet.code.app.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import  org.springframework.stereotype.Service;
+
+import hexlet.code.app.dto.TaskStatusCreateDTO;
+import hexlet.code.app.dto.TaskStatusDTO;
+import hexlet.code.app.dto.TaskStatusUpdateDTO;
+import hexlet.code.app.exception.ResourceNotFoundException;
+import hexlet.code.app.mapper.TaskStatusMapper;
+import hexlet.code.app.repository.TaskStatusRepository;
+
+@Service
+public class TaskStatusService {
+    @Autowired
+    private TaskStatusRepository repository;
+
+    @Autowired
+    private TaskStatusMapper mapper;
+
+    public List<TaskStatusDTO> findAll() {
+        var models= repository.findAll();
+        var result = models.stream()
+                .map(mapper :: map)
+                .toList();
+        return result;
+    }
+
+    public TaskStatusDTO findById(Long id) {
+        var model = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                "Task status with id: " + id + "not found"));
+        return mapper.map(model);
+    }
+
+    public TaskStatusDTO create(TaskStatusCreateDTO data) {
+        var model = mapper.map(data);
+        repository.save(model);
+        return mapper.map(model);
+    }
+
+    public TaskStatusDTO update(Long id, TaskStatusUpdateDTO data) {
+        var model = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                "Task status with id: " + id + "not found"));
+        mapper.update(data, model);
+        repository.save(model);
+        return mapper.map(model);
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+}
