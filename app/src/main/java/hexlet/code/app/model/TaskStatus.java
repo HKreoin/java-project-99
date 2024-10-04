932ec12a9f1892fb450a40154f7032a11a4b801c
Kreoin <steamuser85@yandex.ru>
2024-10-05 00:16:16 +0300

    modify TaskStatus model

--- app/src/main/java/hexlet/code/app/model/TaskStatus.java
+++ app/src/main/java/hexlet/code/app/model/TaskStatus.java
@@ -2,6 +2,7 @@ package hexlet.code.app.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -10,31 +11,39 @@ import jakarta.validation.constraints.Size;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
+
 import static jakarta.persistence.GenerationType.IDENTITY;
 import java.time.LocalDate;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Table(name = "task_statuses")
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class TaskStatus {
+@ToString(includeFieldNames = true, onlyExplicitlyIncluded = true)
+@EntityListeners(AuditingEntityListener.class)
+public class TaskStatus implements BaseEntity {
 
     @Id
     @GeneratedValue(strategy = IDENTITY)
     @EqualsAndHashCode.Include
+    @ToString.Include
     private Long id;
 
     @NotBlank
     @Size(min = 1)
+    @ToString.Include
     private String name;
 
     @NotBlank
     @Column(unique = true)
     @Size(min = 1)
+    @ToString.Include
     private String slug;
 
     @CreatedDate
