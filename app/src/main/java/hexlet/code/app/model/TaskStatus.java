deda3a0130ee0a83a7fa0dd985f811f49ccf011d
Kreoin <steamuser85@yandex.ru>
2024-10-05 20:30:00 +0300

    add links O2M to TaskStatus and User for Task entity

--- app/src/main/java/hexlet/code/app/model/TaskStatus.java
+++ app/src/main/java/hexlet/code/app/model/TaskStatus.java
@@ -1,10 +1,12 @@
 package hexlet.code.app.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -15,6 +17,8 @@ import lombok.ToString;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -34,6 +38,19 @@ public class TaskStatus implements BaseEntity {
     @EqualsAndHashCode.Include
     @ToString.Include
     private Long id;
+
+    @OneToMany(mappedBy = "status", cascade = CascadeType.MERGE, orphanRemoval = true)
+    private List<Task> tasks = new ArrayList<>();
+
+    public void addTask(Task task) {
+        tasks.add(task);
+        task.setStatus(this);
+    }
+
+    public void removeTask(Task task) {
+        tasks.remove(task);
+        task.setStatus(null);
+    }
 
     @NotBlank
     @Size(min = 1)
--- app/src/main/java/hexlet/code/app/model/User.java
+++ app/src/main/java/hexlet/code/app/model/User.java
@@ -5,6 +5,7 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -12,11 +13,13 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -38,6 +41,19 @@ public class User implements BaseEntity, UserDetails {
     @ToString.Include
     @EqualsAndHashCode.Include
     private Long id;
+
+    @OneToMany(mappedBy = "assignee", cascade = CascadeType.MERGE, orphanRemoval = true)
+    private List<Task> tasks = new ArrayList<>();
+
+    public void addTask(Task task) {
+        tasks.add(task);
+        task.setAssignee(this);
+    }
+
+    public void removeTask(Task task) {
+        tasks.remove(task);
+        task.setAssignee(null);
+    }
 
     @Email
     @NotBlank
