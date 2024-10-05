a98aecebf8f51d4551c8bbce9abec3ed69d8d88e
Kreoin <steamuser85@yandex.ru>
2024-10-05 20:28:38 +0300

    move User DTOs to user folder

---
+++ app/src/main/java/hexlet/code/app/dto/user/UserCreateDTO.java
@@ -0,0 +1,26 @@
+package hexlet.code.app.dto.user;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserCreateDTO {
+
+    private String firstName;
+
+    private String lastName;
+
+    @Email
+    @NotBlank
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank
+    @Size(min = 3)
+    private String password;
+}
---
+++ app/src/main/java/hexlet/code/app/dto/user/UserUpdateDTO.java
@@ -0,0 +1,13 @@
+package hexlet.code.app.dto.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserUpdateDTO {
+
+    private String firstName;
+
+    private String lastName;
+}
