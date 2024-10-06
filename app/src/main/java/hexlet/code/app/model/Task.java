cdd36430db5c58f4d354e4de4e6a99e9ed9745b3
Kreoin <steamuser85@yandex.ru>
2024-10-06 22:34:54 +0300

    fix Task

--- app/src/main/java/hexlet/code/app/model/Task.java
+++ app/src/main/java/hexlet/code/app/model/Task.java
@@ -5,7 +5,6 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 import java.time.LocalDate;
 
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.Column;
@@ -13,9 +12,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -43,22 +43,22 @@ public class Task implements BaseEntity {
     @ToString.Include
     private Long index;
 
+    @Column(columnDefinition = "TEXT")
     @ToString.Include
     private String description;
 
-    @NotBlank
+    @NotNull
     @ManyToOne
+    @JoinColumn(name = "task_status_id")
     @ToString.Include
-    private TaskStatus status;
+    private TaskStatus taskStatus;
 
     @ManyToOne
+    @JoinColumn(name = "assignee_id")
     @ToString.Include
     private User assignee;
 
     @CreatedDate
     @Column(updatable = false)
     private LocalDate createdAt;
-
-    @LastModifiedDate
-    private LocalDate updatedAt;
 }
