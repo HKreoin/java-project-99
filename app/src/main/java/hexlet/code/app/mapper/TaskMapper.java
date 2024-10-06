6e4777a55490a150fba903eb9bb0b1941592252a
Kreoin <steamuser85@yandex.ru>
2024-10-06 22:37:23 +0300

    refactor TaskMapper

--- app/src/main/java/hexlet/code/app/mapper/TaskMapper.java
+++ app/src/main/java/hexlet/code/app/mapper/TaskMapper.java
@@ -3,13 +3,18 @@ package hexlet.code.app.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.Named;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import hexlet.code.app.dto.task.TaskCreateDTO;
 import hexlet.code.app.dto.task.TaskDTO;
 import hexlet.code.app.dto.task.TaskUpdateDTO;
+import hexlet.code.app.exception.ResourceNotFoundException;
 import hexlet.code.app.model.Task;
+import hexlet.code.app.model.TaskStatus;
+import hexlet.code.app.repository.TaskStatusRepository;
 
 @Mapper(
     componentModel = "spring",
@@ -19,22 +24,39 @@ import hexlet.code.app.model.Task;
 )
 public abstract class TaskMapper {
 
+    //@Autowired
+    //private UserRepository userRepository;
+
+    @Autowired
+    private TaskStatusRepository taskStatusRepository;
+
+    @Mapping(target = "name", source = "title")
+    @Mapping(target = "description", source = "content")
+    @Mapping(target = "taskStatus", source = "status", qualifiedByName = "slugToTaskStatus")
     @Mapping(target = "assignee", source = "assigneeId")
-    @Mapping(target = "status", source = "statusId")
     public abstract Task map(TaskCreateDTO data);
 
-    @Mapping(target = "assignee", source = "assigneeId")
-    @Mapping(target = "status", source = "statusId")
-    public abstract Task map(TaskUpdateDTO data);
-
+    @Mapping(target = "title", source = "name")
+    @Mapping(target = "content", source = "description")
+    @Mapping(target = "status", source = "taskStatus.slug")
     @Mapping(target = "assigneeId", source = "assignee.id")
-    @Mapping(target = "statusId", source = "status.id")
     public abstract TaskDTO map(Task model);
 
-    @Mapping(target = "assignee.id", source = "assigneeId")
-    @Mapping(target = "status.id", source = "statusId")
+    @Mapping(target = "name", source = "title")
+    @Mapping(target = "description", source = "content")
+    @Mapping(target = "taskStatus", source = "status", qualifiedByName = "slugToTaskStatus")
+    @Mapping(target = "assignee", source = "assigneeId")
     public abstract Task map(TaskDTO data);
 
+    @Mapping(target = "name", source = "title")
+    @Mapping(target = "description", source = "content")
+    @Mapping(target = "taskStatus", source = "status", qualifiedByName = "slugToTaskStatus")
+    @Mapping(target = "assignee", source = "assigneeId")
     public abstract void update(TaskUpdateDTO update, @MappingTarget Task target);
 
+    @Named("slugToTaskStatus")
+    public TaskStatus slugToTaskStatus(String slug) {
+        return taskStatusRepository.findBySlug(slug).orElseThrow(
+                () -> new ResourceNotFoundException("TaskStatus with slug " + slug + " not found"));
+    }
 }
