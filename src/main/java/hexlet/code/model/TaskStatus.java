4f2bd461e88fadf91abc737a9e7c10e849aeb322
Kreoin <steamuser85@yandex.ru>
2024-10-08 22:39:48 +0300

    delete unused import in TaskStatus entity

--- src/main/java/hexlet/code/model/TaskStatus.java
+++ src/main/java/hexlet/code/model/TaskStatus.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.CascadeType;
